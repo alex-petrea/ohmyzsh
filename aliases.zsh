2e26a50ffb029268f9edbf1523df224a04dad530
Alex Petrea <alexcpetrea@gmail.com>
2024-12-15 22:14:28 +0000

    u

--- aliases.zsh
+++ aliases.zsh
@@ -5,7 +5,7 @@ alias c='sail composer '
 alias u='sail php artisan test '
 alias uf='u --filter '
 alias gs='git status'
-alias mk='a make:$(name)'
 alias adda='vi ~/.oh-my-zsh/custom/aliases.zsh'
 alias source_profile='source ~/.zshrc'
 alias his='history'
+
